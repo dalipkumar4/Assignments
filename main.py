4c9ec5f0a617b29c228dea31a712898509fdce89
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-04-26 18:36:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+I) Write a Program to perform the following tasks (10 Marks)
+a. Create a database SELECTION_DB
+b. Set connection with MySQL.connector.connect.
+c. Create a table STUDENT_SELECTION in database SELECTION_DB with following data
+FIRST_NAME,LAST_NAME,AGE,GENDER, SCORE.
+d. change table structure / (add, edit, remove column of a table) at run time
+i. add a column address in the STUDENT_SELECTION table.
+ii. execute SQL INSERT statement to create a record into STUDENT_SELECTION table
+iii. run the query to updates all the records having GENDER as 'M', and increase AGE of
+all the males by one year.
+iv. delete all the records from STUDENT_SELECTION Table where AGE is less than 18
+.
+  
+II) Write a program in Python to print the cube of the numbers present in the list, by using
+map( ) function.
+16
+III) Write Python code to read a dataset (may be CSV file) and print all features i.e. columns of the
+dataset. Determine the descriptive statistics i.e. Maximum, Minimum Mean Median, Count,
+Variance, Standard Deviation, etc. of the numeric features like age, salary, etc., that may be present
+in the dataset.
