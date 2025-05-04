e1a5565f4f2267427ace82847d21b4dbb4cca970
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-04 16:51:52 +0530

    Create 4.py

---
+++ Python Practice/4.py
@@ -0,0 +1,6 @@
+# Q4.Write a program using dictionary for print week days?
+
+days = {1:'Monday',2:'Tuesday',3:'Wednesday', 4:'Thursday', 5:'Friday',6:'Saturday',7:'Sunday'}
+
+for i in days:
+    print(days[i])
