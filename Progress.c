06c9f5635c0d162bd2196a5fea7cf6ed38aa2026
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-06 11:38:25 +0530

    Create Progress.c

---
+++ Progress.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    int i,j,k;
+    // for(i = 1;i<=5;i++){
+    //     for (j = 1; j <= i; j++)
+    //     {
+    //         printf("%d",j);
+    //     }
+    //     printf("\n");
+    // }
+    // OUTPUT:-
+    // 1
+    // 12
+    // 123
+    // 1234
+    // 12345
+
+    // for (i = 5; i >= 1; i--)
+    // {
+    //     for (j = 2; j <=i; j++)
+    //     {
+    //         printf("%d",j);
+    //     }
+    //     printf("\n");
+    // }
+    // OUTPUT:-
+    // 12345
+    // 1234
+    // 123
+    // 12
+    // 1
+    
+
+
+    return 0;
+}
+
+
